4bb0bbfb922b539e8c1a799fbff0162a0548f865
lucky90354 <luckychauhan7478844@gmail.com>
2022-12-19 00:53:10 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request,jsonify
+
+from chat import get_response
+
+app = Flask(__name__)
+
+@app.get("/")
+def index_get():
+    return render_template("base.html")
+
+@app.post("/predict")
+def predict():
+    text = request.get_json().get("message")
+    #TODO: check if text is valid
+    responce =get_response(text)
+    message ={"answer: responce"}
+    return jsonify(message)
+
+
+if __name__=="__main__":
+    app.run(debug=True)
